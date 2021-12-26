959096520719a2c7ec39401af1f307a586404dd8
Effimowsky <o.effimowsky@gmail.com>
2021-12-26 17:28:51 +0400

    create index.html file and add basic code andd create app.js file

---
+++ app.js
@@ -0,0 +1,20 @@
+// Davaleba-1
+
+const Array = [1, 2, 3, 4, 5];
+let sum = 0;
+for (let i = 0; i < Array.length; i++) {
+  sum += Array[i];
+}
+console.log(sum);
+
+// Davaleba-2
+
+const user = [
+  Name: "Omar",
+  Age: 26,
+  Adress: "Tbilisi",
+];
+
+// Davaleba-3
+
+
